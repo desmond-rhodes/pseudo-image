f93a4f1e7abac94dc436798fb863c5f47b871257
Desmond Rhodes <desmond.rhodes@outlook.com>
2022-07-04 05:25:43 +0700

    Setup window

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char* argv[]) {
 	std::ios_base::sync_with_stdio(false);
@@ -10,6 +12,44 @@ int main(int argc, char* argv[]) {
 	for (auto const& i : args)
 		std::cout << i << ' ';
 	std::cout << '\n';
+
+	if (!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+	GLFWwindow* window = glfwCreateWindow(960, 720, "Hello World", NULL, NULL);
+
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (gl3wInit())
+		return -1;
+
+	if (!gl3wIsSupported(4, 5))
+		return -1;
+
+	std::cout
+		<< "OpenGL " << glGetString(GL_VERSION)
+		<< ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION)
+		<< '\n';
+
+	std::cout << std::flush;
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	std::cout << "Hello, world!\n";
 	return 0;
--- makefile
+++ makefile
@@ -1,20 +1,34 @@
 OUT := image
-OBJS := main.o
+OBJS := main.o lib/gl3w.o
+DEPS := $(OBJS:%.o=%.d)
 
+CC := x86_64-w64-mingw32-gcc-posix
+CFLAGS := -std=c17
+
+CXX := x86_64-w64-mingw32-g++-posix
 CXXFLAGS := -std=c++17
+
+CPPFLAGS := -Iinclude
+
+LDFLAGS := -Llib
+LDLIBS := -lglfw3 -lgdi32 -static-libgcc -static-libstdc++ -static -lpthread
 
 DEPFLAGS = -MT $@ -MMD -MP -MF $*.d
 
 $(OUT): $(OBJS)
-	$(LINK.cc) $^ $(LOADLIBES) $(LDLIBS) -o $@
+	$(LINK.cc) $^ $(LOADLIBES) $(LDLIBS) -o .$@
+	mv .$@ $@
+
+%.o: %.c
+	$(COMPILE.c) $(DEPFLAGS) $(OUTPUT_OPTION) $<
 
 %.o: %.cc
 	$(COMPILE.cc) $(DEPFLAGS) $(OUTPUT_OPTION) $<
 
 .PHONY: clean
 clean:
-	rm -f *.d
-	rm -f *.o
+	rm -f $(DEPS)
+	rm -f $(OBJS)
 	rm -f $(OUT)
 
-include $(wildcard $(OBJS:%.o=%.d))
+include $(wildcard $(DEPS))
