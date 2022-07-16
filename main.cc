2d61eb31c2c6a4f12c9a26839cb4e5152a7d9ecf
Desmond Rhodes <desmond.rhodes@outlook.com>
2022-07-16 09:14:41 +0700

    Remove exception, string and vector; Hoist shader

--- main.cc
+++ main.cc
@@ -1,29 +1,41 @@
 #include <GL/gl3w.h>
+#include <new>
 
 class {
 public:
+	GLuint* data {nullptr};
+	size_t w;
+	size_t h;
+
 	bool renew(size_t w, size_t h) {
 		if (data == nullptr || w != this->w || h != this->h) {
+			auto allocate {new (std::nothrow) GLuint[w*h]};
+			if (!allocate)
+				return false;
 			delete[] data;
+
+			data = allocate;
 			this->w = w;
 			this->h = h;
-			data = new GLuint[w*h];
+
 			auto y {static_cast<int>(h)/-2};
-			for (size_t j {0}; j < h; ++j) {
+			size_t j {0};
+			while (j < h) {
 				auto x {static_cast<int>(w)/-2};
-				for (size_t i {0}; i < w; ++i) {
+				size_t i {0};
+				while (i < w) {
 					data[j*w+i] = fragment(x, y);
 					x += 1;
+					i += 1;
 				}
 				y += 1;
+				j += 1;
 			}
+
 			return true;
 		}
 		return false;
 	}
-	GLuint* data {nullptr};
-	size_t w;
-	size_t h;
 
 private:
 	GLuint fragment(int x, int y) {
@@ -67,47 +79,109 @@ private:
 }
 image;
 
-#include <iostream>
-#include <vector>
-#include <string>
+#include <optional>
+#include <ostream>
+#include <fstream>
+
+char* file_content(char const* const filename, std::ostream& os) {
+	std::ifstream file;
+	file.open(filename, std::ifstream::in);
+	if (file.fail()) {
+		os << "Unable to open file " << filename << "\n";
+		return nullptr;
+	}
+	file.seekg(0, std::ios_base::end);
+	auto const size {static_cast<size_t>(file.tellg())};
+	auto content {new (std::nothrow) char[size+1]()};
+	if (!content) {
+		os << "Unable to allocate buffer of size " << size << " bytes to load file " << filename << ".\n";
+		return nullptr;
+	}
+	file.seekg(0, std::ios_base::beg);
+	file.read(content, size);
+	if (file.fail()) {
+		delete[] content;
+		os << "Unable to read file " << filename << "\n";
+		return nullptr;
+	}
+	return content;
+}
+
+GLuint shader_compile(GLenum type, char const* const src, std::ostream& os) {
+	auto const obj {glCreateShader(type)};
+	if (!obj)
+		return 0;
+	glShaderSource(obj, 1, &src, nullptr);
+	glCompileShader(obj);
+	GLint status;
+	glGetShaderiv(obj, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE) {
+		GLint length;
+		glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &length);
+		auto log {new (std::nothrow) char[length+1]};
+		if (!log) {
+			os << "Unable to allocate buffer of size " << length+1 << " bytes to load error log from compiling shader.\n";
+			return 0;
+		}
+		glGetShaderInfoLog(obj, length, nullptr, log);
+		os << log << '\n';
+		delete[] log;
+		glDeleteShader(obj);
+		return 0;
+	}
+	return obj;
+}
+
+GLuint shader_link(size_t n, GLuint const obj[], std::ostream& os) {
+	auto const pro {glCreateProgram()};
+	if (!pro)
+		return 0;
+	for (size_t i {0}; i < n; ++i)
+		glAttachShader(pro, obj[i]);
+	glLinkProgram(pro);
+	GLint status;
+	glGetProgramiv(pro, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE) {
+		GLint length;
+		glGetProgramiv(pro, GL_INFO_LOG_LENGTH, &length);
+		auto log {new (std::nothrow) char[length+1]};
+		if (!log) {
+			os << "Unable to allocate buffer of size " << length+1 << " bytes to load error log from linking shader.\n";
+			return 0;
+		}
+		glGetProgramInfoLog(pro, length, nullptr, log);
+		os << log << '\n';
+		delete[] log;
+		glDeleteProgram(pro);
+		return 0;
+	}
+	for (size_t i {0}; i < n; ++i)
+		glDetachShader(pro, obj[i]);
+	return pro;
+}
+
+#include <functional>
+
+class cleanup {
+public:
+	cleanup(std::function<void()> p) : f {p} {}
+	~cleanup() { f(); }
+private:
+	std::function<void()> f;
+};
+
 #include <GLFW/glfw3.h>
-#include <stdexcept>
+#include <iostream>
 #include <chrono>
 #include <thread>
-
-int pseudo_image();
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(nullptr);
-	try {
-		return pseudo_image();
-	}
-	catch (std::exception const& e) {
-		std::cerr << e.what() << '\n';
-		return -1;
-	}
-	catch (...) {
-		std::cerr << "Uncaught exception of unknown type was encountered.\n";
-		return -1;
-	}
-}
 
-std::string file_content(std::string const&);
-GLuint shader_compile(GLenum, std::string const&);
-GLuint shader_link(std::vector<GLuint> const&);
-
-int pseudo_image() {
-	struct glfwHandle {
-		glfwHandle() {
-			if (!glfwInit())
-				throw std::runtime_error("glfwHandle");
-		}
-		~glfwHandle() {
-			glfwTerminate();
-		}
-	}
-	use_glfw;
+	if (!glfwInit())
+		return -1;
+	cleanup c_glfw {[]{ glfwTerminate(); }};
 
 	struct winfo_t {
 		char const* title {"Pseudo Image"};
@@ -134,25 +208,38 @@ int pseudo_image() {
 		glfwMakeContextCurrent(window);
 		if (gl3wInit())
 			return -1;
-		std::cout
-			<< "OpenGL " << glGetString(GL_VERSION)
-			<< ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION)
-			<< '\n' << std::flush;
+		std::cout << "OpenGL " << glGetString(GL_VERSION) << ", GLSL " << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
 	}
 
-	GLuint shader;
+	GLuint sha;
 	{
-		auto const vert_src {file_content("shader.vert")};
-		auto const frag_src {file_content("shader.frag")};
-		auto const vert_obj {shader_compile(GL_VERTEX_SHADER  , vert_src)};
-		auto const frag_obj {shader_compile(GL_FRAGMENT_SHADER, frag_src)};
-		shader = shader_link({vert_obj, frag_obj});
-		glDeleteShader(vert_obj);
-		glDeleteShader(frag_obj);
-	}
-	glUseProgram(shader);
+		size_t constexpr n {2};
 
-	auto const uniform_tex {glGetUniformLocation(shader, "tex")};
+		char const* const filename[] {"shader.vert", "shader.frag"};
+		GLenum const type[] {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER};
+
+		GLuint obj[n];
+		for (size_t i {0}; i < n; ++i) {
+			auto const src {file_content(filename[i], std::cerr)};
+			if (!src)
+				return -1;
+			auto const o {shader_compile(type[i], src, std::cerr)};
+			if (!o)
+				return -1;
+			obj[i] = o;
+			delete[] src;
+		}
+
+		sha = shader_link(2, obj, std::cerr);
+		if (!sha)
+			return -1;
+
+		for (size_t i {0}; i < n; ++i)
+			glDeleteShader(obj[i]);
+	}
+	glUseProgram(sha);
+
+	auto const uniform_tex {glGetUniformLocation(sha, "tex")};
 
 	GLuint sam;
 	GLuint vao;
@@ -169,12 +256,12 @@ int pseudo_image() {
 		glGenVertexArrays(1, &vao);
 		glBindVertexArray(vao);
 
-		auto const attrib_position {glGetAttribLocation(shader, "vPosition")};
+		auto const attrib_position {glGetAttribLocation(sha, "vPosition")};
 		glEnableVertexAttribArray(attrib_position);
 		glVertexAttribFormat(attrib_position, 2, GL_FLOAT, GL_FALSE, 0);
 		glVertexAttribBinding(attrib_position, bindingindex);
 
-		auto const attrib_texcoord {glGetAttribLocation(shader, "vTexCoord")};
+		auto const attrib_texcoord {glGetAttribLocation(sha, "vTexCoord")};
 		glEnableVertexAttribArray(attrib_texcoord);
 		glVertexAttribFormat(attrib_texcoord, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat));
 		glVertexAttribBinding(attrib_texcoord, bindingindex);
@@ -194,24 +281,26 @@ int pseudo_image() {
 		void resize(GLsizei w, GLsizei h) {
 			this->w = w;
 			this->h = h;
-			glDeleteTextures(1, &name);
-			glCreateTextures(GL_TEXTURE_2D, 1, &name);
-			glTextureStorage2D(name, 1, GL_RGBA8, w, h);
+			glDeleteTextures(1, &id);
+			glCreateTextures(GL_TEXTURE_2D, 1, &id);
+			glTextureStorage2D(id, 1, GL_RGBA8, w, h);
 		}
 		void bind(GLuint uniform) {
-			glBindTextureUnit(uniform, name);
+			glBindTextureUnit(uniform, id);
 		}
 		void data(size_t w, size_t h, GLuint const* image) {
-			glTextureSubImage2D(name, 0, 0, 0, w, h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, image);
+			glTextureSubImage2D(id, 0, 0, 0, w, h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, image);
 		}
-		GLuint name {0};
-		GLsizei w;
-		GLsizei h;
+		GLuint id {0};
+		GLsizei w {0};
+		GLsizei h {0};
 	}
 	texture;
 
 	std::chrono::microseconds const r_limit {16666}; /* 60Hz */
 	auto r_last {std::chrono::steady_clock::now()};
+
+	std::cout << std::flush;
 
 	while (!glfwWindowShouldClose(window)) {
 		if (image.renew(winfo.w, winfo.h)) {
@@ -229,61 +318,4 @@ int pseudo_image() {
 	}
 
 	return 0;
-}
-
-#include <fstream>
-
-std::string file_content(std::string const& name) {
-	std::ifstream file;
-	file.open(name, std::ifstream::in);
-	file.seekg(0, std::ios_base::end);
-	std::string content(file.tellg(), '\0');
-	file.seekg(0, std::ios_base::beg);
-	file.read(content.data(), content.size());
-	file.close();
-	return content;
-}
-
-GLuint shader_compile(GLenum type, std::string const& src) {
-	auto const obj {glCreateShader(type)};
-	if (!obj)
-		throw std::runtime_error("shader_compile");
-	GLchar const* v_src[] {src.data()};
-	glShaderSource(obj, 1, v_src, nullptr);
-	glCompileShader(obj);
-	GLint status;
-	glGetShaderiv(obj, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE) {
-		GLint length;
-		glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &length);
-		std::string log(length, '\0');
-		glGetShaderInfoLog(obj, length, nullptr, log.data());
-		std::cerr << log << '\n';
-		glDeleteShader(obj);
-		throw std::runtime_error("shader_compile");
-	}
-	return obj;
-}
-
-GLuint shader_link(std::vector<GLuint> const& obj) {
-	auto const pro {glCreateProgram()};
-	if (!pro)
-		throw std::runtime_error("shader_link");
-	for (auto const& i : obj)
-		glAttachShader(pro, i);
-	glLinkProgram(pro);
-	GLint status;
-	glGetProgramiv(pro, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE) {
-		GLint length;
-		glGetProgramiv(pro, GL_INFO_LOG_LENGTH, &length);
-		std::string log(length, '\0');
-		glGetProgramInfoLog(pro, length, nullptr, log.data());
-		std::cerr << log << '\n';
-		glDeleteProgram(pro);
-		throw std::runtime_error("shader_link");
-	}
-	for (auto const& i : obj)
-		glDetachShader(pro, i);
-	return pro;
 }
